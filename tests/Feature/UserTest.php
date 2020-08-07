2c35721552ddb44fafdf7ade9f9446956a8346bd
leejk118 <leejk118@gmail.com>
2020-08-07 17:55:02 +0900

    Add User Feature Test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,17 +2,44 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Http\Middleware\VerifyCsrfToken;
+use App\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
+    use DatabaseTransactions, WithFaker;
 
-    public function test_a_user_cannot_login_while_logged_in()
+    public function testRegisterValidation()
     {
-        $response = $this->get('/');
+        $this->withoutMiddleware(VerifyCsrfToken::class);
 
-        $response->assertStatus(302);
+        $this->post(route('users.store'), [
+            'name' => '',
+            'email' => $this->faker->email,
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ])->assertSessionHasErrors('name');
+
+        $this->post(route('users.store'), [
+            'name' => $this->faker->name,
+            'email' => '',
+            'password' => 'password',
+            'password_confirmation' => 'password'
+        ])->assertSessionHasErrors('email');
+
+        $this->post(route('users.store'), [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'password' => 'password',
+            'password_confirmation' => 'passwords'
+        ])->assertSessionHasErrors('password');
+    }
+
+    public function test()
+    {
+
     }
 }
